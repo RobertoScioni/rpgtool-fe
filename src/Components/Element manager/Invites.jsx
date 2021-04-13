ec63c03a940fc1c3e99b1900cc65334c362641c8
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-04-13 17:00:16 +0200

    Update Invites.jsx

--- src/Components/Element manager/Invites.jsx
+++ src/Components/Element manager/Invites.jsx
@@ -152,13 +152,13 @@ const Campaign = (props) => {
 				</div>
 				<div id="save" className="ml-1" onClick={(e) => save()}>
 					<svg
-						class="h-10 w-10 text-green-600"
+						className="h-10 w-10 text-green-600 cursor-pointer transform active:scale-150"
 						viewBox="0 0 24 24"
 						fill="none"
 						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					>
 						<path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
 						<polyline points="17 21 17 13 7 13 7 21" />
