8810051a2ad60c7ac5e675aa587c470ea387d71b
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-04-21 11:42:56 +0200

    updated working documentation/plan for the character sheet

--- src/character sheet format docs.js
+++ src/character sheet format docs.js
@@ -35,6 +35,20 @@ const Zird = {
 	 * a dsc field will have an action to show it's description in a modal
 	 * a min and/or max field will mark and entry as a counter field //must yet be implemented in pages
 	 * an entry with only it's name will be printed as is and have no event attached
+	 * 
+	 * all macros and counters are 'indexed' as variables so if i have the following in a page:
+	 * 	{
+	 * 		name:Strenght
+	 * 		min:-18
+	 * 		value:12
+	 * 		max:+20
+	 * 	},
+	 * 	{
+	 * 		name:Strenght modifier
+	 * 		macro:[floor((@Strenght-10)/2)]
+	 * 	}
+	 * 
+	 * 	then when i resolve  Strenght mofifier i will get [floor((12-10)/2)]
 	 */
 	Pages: {
 		Skills: [
@@ -104,19 +118,87 @@ const Zird = {
 
 /*********
  * to do
- * add support to variables in macros
- * 		why?
- * 			variables whould give the players the ability to define stuff like proficiency and ability modifiers
- * 		how?
- * 			variables will be declared in an object called variables and they will need to have simple values supported by rpg-dice-roller
- * 			a field called Formulas will contain frequently used calculations that reference variables
- * 			being able to simply parse them at rendering whould be optimal but most probably we will have to parse them when we store them
+ * 		search:
+ * 			the ability to search ANYTHING inside the sheet
+ * 		add inventory support
+ * 			how?
+ * 			an inventory should work like a page and be a container of fields
+ * 			fields of an inventory should have a quantity associated with them and a mass/bulk or other accumulator?
+ * 		add notepage support
+ * 			support for markup in notepage
+ * 				visual markup editor in notepages
+ * 		symplify data structure
+ * 			macro and value should become the same field name
+ * 			a fieldtype would be a better way to manage the different kind of fields
+ * 		add "copy paste" support.
+ * 			copy paste support would enable users to copy stuf from character to character or from a library to a character
  */
 
-const exampleWithVariables={
-	Variables: {str:18,dex:10,con:12,int:10,wis:12,cha:15,Proficiency:2,level:1},
-	Formulas:{mInt:"[floor((int-10)/2)]",mStr:"[floor((str-10)/2)]"}
-	Counters:{}
-	Pages:{},
+/*********
+ * theoretical new simplified data structure for a single element
+ */
+const element=
+	{ 
+		'name':'string',//self explanatory, it is the name of the element described
+		'type':'info'||'macro'||'formula'||'field'||'counter',//this is one of the supported datatypes
+		'value':'string'||'integer',//value can be a string for macros and fields or a integer for counters
+		'description':'string',//a long text description that explains the entry
+		'abbreviation':'string',//a shortened name for the entry
+		'pages':[//collection of page object in witch the element appears
+			{//a page element
+				'name':'string',//name of the page
+				'sort':'integer',//manual position of the element in the page
+			}
+		],
+		'min':'integer',//an integer
+		'max':'integer',//an integer
+		'Item':{//any field with an item value is considered an item
+			'qty':'integer',//an integer that represents how many copy of the item are in the inventory
+			'mass':'integer'//the weight of a single copy of the item
+			}
+	}
 
-}
+/*********
+ * the datatypes:
+ * 
+ * the macro datatype represents an interactive rollable element it will generate a macro in the sheet.
+ * 
+ * the formula datatype represents a non interactive math formula it's not rollable but can be referenced as a variable and can reference variables
+ * 		this exists for stuff like ability modifiers
+ * 
+ * the info datatype represents a non interactive item it will produce a simple text in the sheet.
+ * 
+ * the field datatype represents a fillable short string (it could be a condition name or in the case of fate a consequence)
+ * 
+ * the counter datatype represents an interactive 
+ */
+
+/****************************************************************
+ * theoretical simplified sheet
+ */
+
+const sheet={
+	'pages':['Strings'],//an array of strings containing all the page names
+	'bars':['strings'],//an array of strings containing "bar names" a bar is an horizontal container visualized in all the pages
+	'elements':['element']//an array of objects in the structure of elements
+};
+
+/*********
+ * visual structure of the character sheet
+ */
+
+/*************************************
+ * page1*page2*page3*      * FP * HP *
+ *************************************
+ * 				bar1                 *
+ *************************************
+ *				bar2				 *
+ *************************************
+ *			   bar ... n             *
+ *************************************
+ * self arranging elements as defined*
+ * by the page field of the elements *
+ * themselves                        *
+ * 									 *
+ * 									 *
+ *************************************/
